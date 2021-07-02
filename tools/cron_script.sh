a55871933720e8d49a83220db428ea19f688e46d
Manoel Marques <manoel.marques@ibm.com>
2021-07-02 18:40:58 -0400

    Modify cron script to handle token parameter

--- tools/cron_script.sh
+++ tools/cron_script.sh
@@ -11,35 +11,93 @@
 # copyright notice, and modified files need to carry a notice indicating
 # that they have been altered from the originals.
 
-set -o xtrace
-
 # A virtual env names benchmark has been created
 # and has all the qiskit-app-benchmarks requirements-dev.txt
 # dependencies installed
-source /opt/benchmark/bin/activate
 
 if [[ -f /tmp/benchmark.lock ]] ; then
+    echo 'Script still running, file /tmp/benchmark.lock exists.'
     exit 0
 fi
+echo 'Start script ...'
 touch /tmp/benchmark.lock
 
-# qiskit-app-benchmarks was cloned in opt and is checkout to main branch
-pushd /opt/qiskit-app-benchmarks
+cat > /tmp/.git-askpass <<EOF
+$1
+EOF
+export GIT_ASKPASS=/tmp/.git-askpass
+
+source /opt/benchmark/bin/activate
+
+set -e
+
+echo 'qiskit-app-benchmarks was already cloned in opt and is checkout to main branch'
+echo 'qiskit-app-benchmarks has a gh-pages branch with the html contents in it'
+if [ -d /tmp/qiskit-app-benchmarks ]; then
+  rm -rf /tmp/qiskit-app-benchmarks
+fi
+git clone https://qiskit-asv-bot@github.com/Qiskit/qiskit-app-benchmarks.git /tmp/qiskit-app-benchmarks
+
 git pull
+make clean_sphinx
+make html SPHINXOPTS=-W
+
+echo 'Copy main docs'
+
+pushd /tmp/qiskit-app-benchmarks
+git config user.name "Qiskit Application Benchmarks Autodeploy"
+git config user.email "qiskit@qiskit.org"
+git checkout gh-pages
+GLOBIGNORE=.git:finance:machine_learning:nature:optimization
+rm -rf * .*
+popd
+cp -r docs/_build/html/* /tmp/qiskit-app-benchmarks
+unset GLOBIGNORE
+
+pushd /tmp/qiskit-app-benchmarks
+git add .
+# push only if there are changes
+if git diff-index --quiet HEAD --; then
+  echo 'Nothing to commit for the base doc template.'
+else
+  git commit -m "[Benchmarks] Base documentation update"
+fi
+popd
+
 declare -a targets=("finance" "machine_learning" "nature" "optimization")
-echo “Run Benchmarks for domains”
+echo 'Run Benchmarks for domains'
 for target in "${targets[@]}"
 do
   pushd $target
-  date
-  asv run --launch-method spawn --record-samples NEW
-  date
+  if [ -n "$(find benchmarks/* -not -name '__*' | head -1)" ]; then
+    date
+    echo "Run Benchmark for domain $target"
+    asv run --launch-method spawn --record-samples NEW
+    date
+    asv publish
+    rm -rf /tmp/qiskit-app-benchmarks/$target/*
+    cp -r .asv/html/* /tmp/qiskit-app-benchmarks/$target
+  else
+    rm -rf /tmp/qiskit-app-benchmarks/$target/*
+    cp -r ../docs/_build/html/$target/* /tmp/qiskit-app-benchmarks/$target
+    echo "No Benchmark files found for domain $target, run skipped."
+  fi
+  popd
+  pushd /tmp/qiskit-app-benchmarks
+  git add .
+  # push only if there are changes
+  if git diff-index --quiet HEAD --; then
+    echo "Nothing to push for $target."
+  else
+    git commit -m "[Benchmarks $target] Automated documentation update"
+    git push origin gh-pages
+  fi
   popd
 done
-echo “Publishes html to branch gh-pages”
-date
-python tools/publish_html.py -user Qiskit
-date
-popd
 
+echo 'Final Cleanup'
+unset GIT_ASKPASS
+rm /tmp/.git-askpass
+rm -rf /tmp/qiskit-app-benchmarks
 rm /tmp/benchmark.lock
+echo 'End of script.'
