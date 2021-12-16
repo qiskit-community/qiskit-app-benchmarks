f7898bfa9bf676bd4012d3833466a6fe5dbf8af7
Anton Dekusar <adekusar@ie.ibm.com>
2021-12-16 23:22:58 +0000

    base class

--- machine_learning/benchmarks/base_classifier_benchmark.py
+++ machine_learning/benchmarks/base_classifier_benchmark.py
@@ -13,8 +13,15 @@
 """Base for Classifier benchmarks."""
 
 from abc import ABC
-from qiskit import Aer
+from typing import Optional
+
+from qiskit import Aer, QuantumCircuit
+from qiskit.algorithms.optimizers import Optimizer
+from qiskit.circuit.library import ZZFeatureMap, RealAmplitudes
 from qiskit.utils import QuantumInstance
+from qiskit_machine_learning.algorithms import NeuralNetworkClassifier
+from qiskit_machine_learning.neural_networks import CircuitQNN
+
 from .datasets import (
     DATASET_SYNTHETIC_CLASSIFICATION_FEATURES,
     DATASET_SYNTHETIC_CLASSIFICATION_LABELS,
@@ -48,3 +55,70 @@ class BaseClassifierBenchmark(ABC):
                 "labels": DATASET_IRIS_CLASSIFICATION_LABELS,
             },
         }
+
+    def _construct_qnn_synthetic(
+        self, quantum_instance_name: str, optimizer: Optional[Optimizer] = None
+    ) -> NeuralNetworkClassifier:
+
+        output_shape = 2
+        num_inputs = 2
+
+        # parity maps bitstrings to 0 or 1
+        def parity(x):
+            return f"{x:b}".count("1") % 2
+
+        # construct feature map
+        feature_map = ZZFeatureMap(num_inputs)
+
+        # construct ansatz
+        ansatz = RealAmplitudes(num_inputs, reps=1)
+
+        # construct quantum circuit
+        qc = QuantumCircuit(num_inputs)
+        qc.append(feature_map, range(num_inputs))
+        qc.append(ansatz, range(num_inputs))
+
+        # construct QNN
+        self.circuit_qnn = CircuitQNN(
+            circuit=qc,
+            input_params=feature_map.parameters,
+            weight_params=ansatz.parameters,
+            interpret=parity,
+            output_shape=output_shape,
+            quantum_instance=self.backends[quantum_instance_name],
+        )
+
+        model = NeuralNetworkClassifier(neural_network=self.circuit_qnn, optimizer=optimizer)
+        return model
+
+    def _construct_qnn_iris(
+        self, quantum_instance_name: str, optimizer: Optional[Optimizer] = None
+    ) -> NeuralNetworkClassifier:
+        output_shape = 3
+        num_inputs = 4
+
+        # creating feature map
+        feature_map = ZZFeatureMap(num_inputs)
+
+        # creating ansatz
+        ansatz = RealAmplitudes(num_inputs)
+
+        qc = QuantumCircuit(num_inputs)
+        qc.append(feature_map, range(num_inputs))
+        qc.append(ansatz, range(num_inputs))
+
+        def three_class(x):
+            return f"{x:b}".count("1") % 3
+
+        # construct QNN
+        circuit_qnn = CircuitQNN(
+            circuit=qc,
+            input_params=feature_map.parameters,
+            weight_params=ansatz.parameters,
+            interpret=three_class,
+            output_shape=output_shape,
+            quantum_instance=self.backends[quantum_instance_name],
+        )
+
+        model = NeuralNetworkClassifier(neural_network=circuit_qnn,optimizer=optimizer)
+        return model
