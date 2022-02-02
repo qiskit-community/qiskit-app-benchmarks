ed248b916f0afc3810dc8f374dd4ba8a0dfc1b0c
Desiree <desireevogtlee@gmail.com>
2022-02-02 22:51:10 +1000

    Addition of new ML regression benchmarks  (#28)
    * added ML regression time benchmarks
    
    * added iris dataset subset
    
    * added version property to benchmarks
    
    * fixed lint issues
    
    * added labels to classifier synthetic data
    
    * changed to directly import labels from dataset
    
    * added diabetes dataset and import data directly from sklearn
    
    * added classification benchmark for confusion matrix metrics
    
    * added iris dataset to classification timed benchmarks
    
    * rremoved classification benchmark changes - move to branch
    
    * changed name dataset_1, removed random sampling dataset, fixed params
    
    * removed changes to classification benchmarks
    
    * added CCPP dataset and benchmarks
    
    * added regression score benchmarks
    
    * added ccpp dataset to timed benchmarks
    
    * add pandas to requirements
    
    * added pandas dependency to the asv conf
    
    * standardised dataset naming
    
    * fixed linting error
    
    * new regression synthetic dataset
    
    * cache model
    
    * asv setup cache
    
    * fix f string, add setup_cache
    
    * simplify and speed up regressor benchmarks
    
    * fix spell
    
    * add algorithm_globals
    
    * fix copyright
    
    * Update machine_learning/benchmarks/base_regressor_benchmark.py
    
    Co-authored-by: dlasecki <dal@zurich.ibm.com>
    
    * Update machine_learning/benchmarks/base_regressor_benchmark.py
    
    Co-authored-by: dlasecki <dal@zurich.ibm.com>
    
    * Update machine_learning/benchmarks/base_regressor_benchmark.py
    
    Co-authored-by: dlasecki <dal@zurich.ibm.com>
    
    * Update machine_learning/benchmarks/base_regressor_benchmark.py
    
    Co-authored-by: dlasecki <dal@zurich.ibm.com>
    
    * Update machine_learning/benchmarks/base_regressor_benchmark.py
    
    Co-authored-by: dlasecki <dal@zurich.ibm.com>
    
    * Update machine_learning/benchmarks/opflow_qnn_regressor_benchmark.py
    
    Co-authored-by: dlasecki <dal@zurich.ibm.com>
    
    * Update machine_learning/benchmarks/opflow_qnn_regressor_fit_benchmark.py
    
    Co-authored-by: dlasecki <dal@zurich.ibm.com>
    
    * Update machine_learning/benchmarks/opflow_qnn_regressor_fit_benchmark.py
    
    Co-authored-by: dlasecki <dal@zurich.ibm.com>
    
    * Update machine_learning/benchmarks/opflow_qnn_regressor_benchmark.py
    
    Co-authored-by: dlasecki <dal@zurich.ibm.com>
    
    * Update machine_learning/benchmarks/opflow_qnn_regressor_benchmark.py
    
    Co-authored-by: dlasecki <dal@zurich.ibm.com>
    
    * Update machine_learning/benchmarks/opflow_qnn_regressor_benchmark.py
    
    Co-authored-by: dlasecki <dal@zurich.ibm.com>
    
    * Update machine_learning/benchmarks/opflow_qnn_regressor_benchmark.py
    
    Co-authored-by: dlasecki <dal@zurich.ibm.com>
    
    * code review
    
    * fix mypy
    
    * fix capital letters
    
    * fix copyright, spelling
    
    * more on code review
    
    * Update machine_learning/benchmarks/base_regressor_benchmark.py
    
    Co-authored-by: dlasecki <dal@zurich.ibm.com>
    
    * Update machine_learning/benchmarks/datasets.py
    
    Co-authored-by: dlasecki <dal@zurich.ibm.com>
    
    * code review
    
    Co-authored-by: Manoel Marques <Manoel.Marques@ibm.com>
    Co-authored-by: Anton Dekusar <adekusar@ie.ibm.com>
    Co-authored-by: Anton Dekusar <62334182+adekusar-drl@users.noreply.github.com>
    Co-authored-by: dlasecki <dal@zurich.ibm.com>

--- machine_learning/benchmarks/datasets.py
+++ machine_learning/benchmarks/datasets.py
@@ -12,7 +12,12 @@
 
 """Dataset definitions for machine learning benchmarks."""
 
+import pathlib
+from typing import Tuple
+
 import numpy as np
+import pandas as pd
+
 
 DATASET_SYNTHETIC_CLASSIFICATION_FEATURES = np.array(
     [
@@ -47,3 +52,84 @@ DATASET_SYNTHETIC_CLASSIFICATION_FEATURES = np.array(
 DATASET_SYNTHETIC_CLASSIFICATION_LABELS = np.array(
     [0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 0, 0, 1, 0, 0, 0, 1, 0, 1, 1, 0, 0, 1, 1]
 )
+
+
+# Synthetic dataset for regression is generated as a noisy sine wave.
+# For more details please refer to the "Neural Network Classifier & Regressor" tutorial
+# in Qiskit Machine Learning.
+DATASET_SYNTHETIC_REGRESSION_FEATURES = np.array(
+    [
+        [-1.0],
+        [-0.61017247],
+        [0.3437984],
+        [-0.57425039],
+        [-0.15067904],
+        [0.54669445],
+        [-0.16936095],
+        [-0.90157044],
+        [0.37708312],
+        [0.99143454],
+        [-0.49085348],
+        [0.53117172],
+        [1.0],
+        [0.02066802],
+        [-0.6564818],
+        [-0.76721681],
+        [-0.64475023],
+        [0.43324292],
+        [0.71212452],
+        [0.80997021],
+        [-0.07795483],
+        [0.32083733],
+        [0.17454994],
+        [0.56743491],
+        [-0.12885006],
+    ]
+)
+
+DATASET_SYNTHETIC_REGRESSION_LABELS = np.array(
+    [
+        [-0.07742259],
+        [-1.01861621],
+        [0.9456131],
+        [-1.15321842],
+        [-0.60170763],
+        [1.03743838],
+        [-0.53921747],
+        [-0.53976918],
+        [0.99681555],
+        [0.29210661],
+        [-1.09853086],
+        [0.89345683],
+        [0.12046636],
+        [-0.03882522],
+        [-0.88622102],
+        [-0.83758335],
+        [-0.98542812],
+        [1.07553076],
+        [0.70927951],
+        [0.64405885],
+        [-0.36920025],
+        [0.89642704],
+        [0.41763132],
+        [1.13432333],
+        [-0.56352804],
+    ]
+)
+
+
+def load_ccpp() -> Tuple[np.ndarray, np.ndarray]:
+    """
+    Loads the Combined Cycle Power Plant dataset. See the `UCI Machine Learning Repository
+    <https://archive.ics.uci.edu/ml/datasets/Combined+Cycle+Power+Plant>`_ web site for more
+    details.
+
+    Returns:
+        a tuple with features and labels as numpy arrays.
+    """
+    # the benchmarks are run in a temp directory, but we have to reference a file with the dataset.
+    abs_path = pathlib.Path(__file__).parent.resolve()
+    ccpp_df = pd.read_csv(f"{abs_path}/CCPP_data.csv")
+    ccpp_features = ccpp_df[["AT", "V", "AP", "RH"]].to_numpy()
+    ccpp_labels = ccpp_df["PE"].to_numpy()
+    return ccpp_features, ccpp_labels
---
+++ machine_learning/benchmarks/opflow_qnn_regressor_fit_benchmark.py
@@ -0,0 +1,83 @@
+# This code is part of Qiskit.
+#
+# (C) Copyright IBM 2021, 2022.
+#
+# This code is licensed under the Apache License, Version 2.0. You may
+# obtain a copy of this license in the LICENSE.txt file in the root directory
+# of this source tree or at http://www.apache.org/licenses/LICENSE-2.0.
+#
+# Any modifications or derivative works of this code must retain this
+# copyright notice, and modified files need to carry a notice indicating
+# that they have been altered from the originals.
+"""Neural network regressor benchmarks."""
+
+from itertools import product
+from timeit import timeit
+
+from qiskit.algorithms.optimizers import COBYLA, L_BFGS_B, NELDER_MEAD
+
+from .base_regressor_benchmark import (
+    BaseRegressorBenchmark,
+    DATASET_SYNTHETIC_REGRESSION,
+    DATASET_CCPP_REGRESSION,
+)
+
+
+class OpflowQnnFitRegressorBenchmarks(BaseRegressorBenchmark):
+    """OpflowQNN regressor benchmarks."""
+
+    version = 1
+    timeout = 1200.0
+    params = (
+        [DATASET_SYNTHETIC_REGRESSION, DATASET_CCPP_REGRESSION],
+        ["qasm_simulator", "statevector_simulator"],
+        ["cobyla", "nelder-mead", "l-bfgs-b"],
+    )
+    param_names = ["dataset", "backend name", "optimizer"]
+
+    def __init__(self) -> None:
+        super().__init__()
+        self.optimizers = {
+            "cobyla": COBYLA(maxiter=100),
+            "nelder-mead": NELDER_MEAD(maxiter=50),
+            "l-bfgs-b": L_BFGS_B(maxiter=20),
+        }
+        self.train_features = None
+        self.train_labels = None
+        self.model = None
+
+    def setup(self, dataset: str, quantum_instance_name: str, optimizer: str):
+        """Set up the benchmark."""
+        self.train_features = self.datasets[dataset]["train_features"]
+        self.train_labels = self.datasets[dataset]["train_labels"]
+
+        if dataset == DATASET_SYNTHETIC_REGRESSION:
+            self.model = self._construct_qnn_synthetic(
+                quantum_instance_name, self.optimizers[optimizer]
+            )
+        elif dataset == DATASET_CCPP_REGRESSION:
+            self.model = self._construct_qnn_ccpp(quantum_instance_name, self.optimizers[optimizer])
+        else:
+            raise ValueError(f"Unsupported dataset: {dataset}")
+
+    # pylint: disable=invalid-name
+    def time_fit_opflow_qnn_regressor(self, _, __, ___):
+        """Time fitting OpflowQNN regressor to data."""
+        self.model.fit(self.train_features, self.train_labels)
+
+
+if __name__ == "__main__":
+    for dataset_name, backend, optimizer_name in product(*OpflowQnnFitRegressorBenchmarks.params):
+        bench = OpflowQnnFitRegressorBenchmarks()
+        try:
+            bench.setup(dataset_name, backend, optimizer_name)
+        except NotImplementedError:
+            continue
+
+        for method in ["time_fit_opflow_qnn_regressor"]:
+            elapsed = timeit(
+                f'bench.{method}("{dataset_name}", "{backend}", "{optimizer_name}")',
+                number=10,
+                globals=globals(),
+            )
+            print(f"{method}:\t{elapsed}")
