5626f0e771dbe0bc205f20f5e43764f1e5f97536
Desiree <desireevogtlee@gmail.com>
2022-02-02 21:24:22 +1000

    Addition of new ML classification benchmarks (#27)
    * added ML regression time benchmarks
    
    * added iris dataset subset
    
    * added version property to benchmarks
    
    * fixed lint issues
    
    * added labels to classifier synthetic data
    
    * changed to directly import labels from dataset
    
    * added diabetes dataset and import data directly from sklearn
    
    * added classification benchmark for confusion matrix metrics
    
    * added iris dataset to classification timed benchmarks
    
    * removed regression benchmark changes - move to branch
    
    * changed name of dataset_1 and removed randomisation from iris sampling
    
    * fixed param names
    
    * fixed iris dataset sampling
    
    * iris dataset sampling
    
    * moved dataset definitions
    
    * fixed lint issues
    
    * added iris dataset to vqc benchmarks
    
    * added iris to circuitqnn fit benchmark
    
    * added iris data to opflow classifier benchmarks
    
    * changed score metrics in classifier benchmarks
    
    * standardised dataset naming
    
    * unifying signatures
    
    * added cohen to pylint dict
    
    * removed forward pass in opflow
    
    * updated feature maps, ansatze and optimizers
    
    * updated score benchmarks
    
    * asv setup cache
    
    * base class
    
    * updated benchmarks
    
    * fix version, vqc
    
    * similar to the regressor benchmark changes
    
    * Update machine_learning/benchmarks/circuit_qnn_base_classifier_benchmark.py
    
    Co-authored-by: dlasecki <dal@zurich.ibm.com>
    
    * Update machine_learning/benchmarks/circuit_qnn_base_classifier_benchmark.py
    
    Co-authored-by: dlasecki <dal@zurich.ibm.com>
    
    * Update machine_learning/benchmarks/circuit_qnn_classifier_benchmark.py
    
    Co-authored-by: dlasecki <dal@zurich.ibm.com>
    
    * Update machine_learning/benchmarks/vqc_benchmark.py
    
    Co-authored-by: dlasecki <dal@zurich.ibm.com>
    
    * removed duplicated words in pylint dict
    
    * Update machine_learning/benchmarks/opflow_qnn_base_classifier_benchmark.py
    
    Co-authored-by: dlasecki <dal@zurich.ibm.com>
    
    * Update machine_learning/benchmarks/vqc_base_benchmark.py
    
    Co-authored-by: dlasecki <dal@zurich.ibm.com>
    
    * Update machine_learning/benchmarks/vqc_base_benchmark.py
    
    Co-authored-by: dlasecki <dal@zurich.ibm.com>
    
    * Update machine_learning/benchmarks/opflow_qnn_classifier_benchmark.py
    
    Co-authored-by: dlasecki <dal@zurich.ibm.com>
    
    * Update machine_learning/benchmarks/opflow_qnn_base_classifier_benchmark.py
    
    Co-authored-by: dlasecki <dal@zurich.ibm.com>
    
    * Update machine_learning/benchmarks/circuit_qnn_base_classifier_benchmark.py
    
    Co-authored-by: dlasecki <dal@zurich.ibm.com>
    
    * Update machine_learning/benchmarks/circuit_qnn_base_classifier_benchmark.py
    
    Co-authored-by: dlasecki <dal@zurich.ibm.com>
    
    * Update machine_learning/benchmarks/circuit_qnn_base_classifier_benchmark.py
    
    Co-authored-by: dlasecki <dal@zurich.ibm.com>
    
    * more on code review
    
    * more on code review
    
    * Update machine_learning/benchmarks/base_classifier_benchmark.py
    
    Co-authored-by: dlasecki <dal@zurich.ibm.com>
    
    * Update machine_learning/benchmarks/base_classifier_benchmark.py
    
    Co-authored-by: dlasecki <dal@zurich.ibm.com>
    
    * code review
    
    Co-authored-by: Manoel Marques <Manoel.Marques@ibm.com>
    Co-authored-by: Anton Dekusar <62334182+adekusar-drl@users.noreply.github.com>
    Co-authored-by: Anton Dekusar <adekusar@ie.ibm.com>
    Co-authored-by: dlasecki <dal@zurich.ibm.com>

--- machine_learning/benchmarks/circuit_qnn_classifier_benchmark.py
+++ machine_learning/benchmarks/circuit_qnn_classifier_benchmark.py
@@ -1,6 +1,6 @@
 # This code is part of Qiskit.
 #
-# (C) Copyright IBM 2021.
+# (C) Copyright IBM 2021, 2022.
 #
 # This code is licensed under the Apache License, Version 2.0. You may
 # obtain a copy of this license in the LICENSE.txt file in the root directory
@@ -9,95 +9,137 @@
 # Any modifications or derivative works of this code must retain this
 # copyright notice, and modified files need to carry a notice indicating
 # that they have been altered from the originals.
-
 """Circuit QNN Classifier benchmarks."""
+
+import pickle
 from itertools import product
 from timeit import timeit
+from typing import Optional
 
 import numpy as np
-from qiskit import QuantumCircuit
-from qiskit.circuit.library import ZZFeatureMap, RealAmplitudes
 from qiskit.algorithms.optimizers import COBYLA
-from qiskit_machine_learning.neural_networks import CircuitQNN
-from qiskit_machine_learning.algorithms.classifiers import NeuralNetworkClassifier
+from qiskit_machine_learning.algorithms import NeuralNetworkClassifier
+from sklearn.metrics import f1_score, precision_score, recall_score
 
-# pylint: disable=redefined-outer-name, invalid-name, attribute-defined-outside-init
-from .base_classifier_benchmark import BaseClassifierBenchmark
+from .circuit_qnn_base_classifier_benchmark import CircuitQnnBaseClassifierBenchmark
+from .base_classifier_benchmark import (
+    DATASET_SYNTHETIC_CLASSIFICATION,
+    DATASET_IRIS_CLASSIFICATION,
+)
 
 
-class CircuitQnnClassifierBenchmarks(BaseClassifierBenchmark):
+class CircuitQnnClassifierBenchmarks(CircuitQnnBaseClassifierBenchmark):
     """Circuit QNN Classifier benchmarks."""
 
-    version = 1
+    version = 2
     timeout = 1200.0
-    params = [["dataset_1"], ["qasm_simulator", "statevector_simulator"]]
-    param_names = ["backend name"]
+    params = [
+        [DATASET_SYNTHETIC_CLASSIFICATION, DATASET_IRIS_CLASSIFICATION],
+        ["qasm_simulator", "statevector_simulator"],
+    ]
+    param_names = ["dataset", "backend name"]
 
-    def __init__(self):
+    def __init__(self) -> None:
         super().__init__()
+        self.train_features: Optional[np.ndarray] = None
+        self.train_labels: Optional[np.ndarray] = None
+        self.test_features: Optional[np.ndarray] = None
+        self.test_labels: Optional[np.ndarray] = None
+        self.model: Optional[NeuralNetworkClassifier] = None
 
-        self.output_shape = 2  # corresponds to the number of classes, possible outcomes of the (
-        # parity) mapping.
+    def setup(self, dataset: str, quantum_instance_name: str) -> None:
+        """Set up the benchmark."""
 
-    def setup(self, dataset, quantum_instance_name):
-        """setup"""
-        self.X = self.datasets[dataset]
-        num_inputs = len(self.X[0])
-        self.y01 = 1 * (np.sum(self.X, axis=1) >= 0)  # in { 0,  1}
+        self.train_features = self.datasets[dataset]["train_features"]
+        self.train_labels = self.datasets[dataset]["train_labels"]
+        self.test_features = self.datasets[dataset]["test_features"]
+        self.test_labels = self.datasets[dataset]["test_labels"]
 
-        # parity maps bitstrings to 0 or 1
-        def parity(x):
-            return f"{x:b}".count("1") % 2
+        if dataset == DATASET_SYNTHETIC_CLASSIFICATION:
+            self.model = self._construct_qnn_classifier_synthetic(
+                quantum_instance_name=quantum_instance_name
+            )
+        elif dataset == DATASET_IRIS_CLASSIFICATION:
+            self.model = self._construct_qnn_classifier_iris(
+                quantum_instance_name=quantum_instance_name
+            )
+        else:
+            raise ValueError(f"Unsupported dataset: {dataset}")
 
-        # construct feature map
-        feature_map = ZZFeatureMap(num_inputs)
+        file_name = f"circuit_qnn_{dataset}_{quantum_instance_name}.pickle"
+        with open(file_name, "rb") as file:
+            self.model._fit_result = pickle.load(file)
 
-        # construct ansatz
-        ansatz = RealAmplitudes(num_inputs, reps=1)
+    def setup_cache(self) -> None:
+        """Cache CircuitQNN fitted model."""
+        for dataset, backend in product(*self.params):
+            train_features = self.datasets[dataset]["train_features"]
+            train_labels = self.datasets[dataset]["train_labels"]
 
-        # construct quantum circuit
-        qc = QuantumCircuit(num_inputs)
-        qc.append(feature_map, range(num_inputs))
-        qc.append(ansatz, range(num_inputs))
+            if dataset == DATASET_SYNTHETIC_CLASSIFICATION:
+                model = self._construct_qnn_classifier_synthetic(
+                    quantum_instance_name=backend, optimizer=COBYLA(maxiter=200)
+                )
+            elif dataset == DATASET_IRIS_CLASSIFICATION:
+                model = self._construct_qnn_classifier_iris(
+                    quantum_instance_name=backend, optimizer=COBYLA(maxiter=200)
+                )
+            else:
+                raise ValueError(f"Unsupported dataset: {dataset}")
 
-        # construct QNN
-        self.circuit_qnn = CircuitQNN(
-            circuit=qc,
-            input_params=feature_map.parameters,
-            weight_params=ansatz.parameters,
-            interpret=parity,
-            output_shape=self.output_shape,
-            quantum_instance=self.backends[quantum_instance_name],
-        )
+            model.fit(train_features, train_labels)
 
-        self.circuit_classifier_fitted = NeuralNetworkClassifier(
-            neural_network=self.circuit_qnn, optimizer=COBYLA()
-        )
-        self.circuit_classifier_fitted.fit(self.X, self.y01)
+            file_name = f"circuit_qnn_{dataset}_{backend}.pickle"
+            with open(file_name, "wb") as file:
+                pickle.dump(model._fit_result, file)
 
+    # pylint: disable=invalid-name
     def time_score_circuit_qnn_classifier(self, _, __):
         """Time scoring CircuitQNN classifier on data."""
-
-        self.circuit_classifier_fitted.score(self.X, self.y01)
+        self.model.score(self.train_features, self.train_labels)
 
     def time_predict_circuit_qnn_classifier(self, _, __):
         """Time predicting with CircuitQNN classifier."""
+        self.model.predict(self.train_features)
 
-        y_predict = self.circuit_classifier_fitted.predict(self.X)
-        return y_predict
+    def track_accuracy_score_circuit_qnn_classifier(self, _, __):
+        """Tracks the overall accuracy of the classification results."""
+        return self.model.score(self.test_features, self.test_labels)
+
+    def track_precision_score_circuit_qnn_classifier(self, _, __):
+        """Tracks the precision score."""
+        predicts = self.model.predict(self.test_features)
+        return precision_score(y_true=self.test_labels, y_pred=predicts, average="micro")
+
+    def track_recall_score_circuit_qnn_classifier(self, _, __):
+        """Tracks the recall score for each class of the classification results."""
+        predicts = self.model.predict(self.test_features)
+        return recall_score(y_true=self.test_labels, y_pred=predicts, average="micro")
+
+    def track_f1_score_circuit_qnn_classifier(self, _, __):
+        """Tracks the f1 score for each class of the classification results."""
+        predicts = self.model.predict(self.test_features)
+        return f1_score(y_true=self.test_labels, y_pred=predicts, average="micro")
 
 
 if __name__ == "__main__":
-    for dataset, backend in product(*CircuitQnnClassifierBenchmarks.params):
-        bench = CircuitQnnClassifierBenchmarks()
+    bench = CircuitQnnClassifierBenchmarks()
+    bench.setup_cache()
+    for dataset_name, backend_name in product(*CircuitQnnClassifierBenchmarks.params):
         try:
-            bench.setup(dataset, backend)
+            bench.setup(dataset_name, backend_name)
         except NotImplementedError:
             continue
 
         for method in (
             "time_score_circuit_qnn_classifier",
             "time_predict_circuit_qnn_classifier",
+            "track_accuracy_score_circuit_qnn_classifier",
+            "track_precision_score_circuit_qnn_classifier",
+            "track_recall_score_circuit_qnn_classifier",
+            "track_f1_score_circuit_qnn_classifier",
         ):
-            elapsed = timeit(f"bench.{method}(None, None)", number=10, globals=globals())
+            elapsed = timeit(
+                f'bench.{method}("{dataset_name}", "{backend_name}")', number=10, globals=globals()
+            )
             print(f"{method}:\t{elapsed}")
--- machine_learning/benchmarks/vqc_fit_benchmark.py
+++ machine_learning/benchmarks/vqc_fit_benchmark.py
@@ -1,6 +1,6 @@
 # This code is part of Qiskit.
 #
-# (C) Copyright IBM 2021.
+# (C) Copyright IBM 2021, 2022.
 #
 # This code is licensed under the Apache License, Version 2.0. You may
 # obtain a copy of this license in the LICENSE.txt file in the root directory
@@ -9,76 +9,91 @@
 # Any modifications or derivative works of this code must retain this
 # copyright notice, and modified files need to carry a notice indicating
 # that they have been altered from the originals.
-
 """Variational Quantum Classifier benchmarks."""
+
 from itertools import product
 from timeit import timeit
+from typing import Optional
 
 import numpy as np
-from qiskit.circuit.library import ZZFeatureMap, RealAmplitudes
 from qiskit.algorithms.optimizers import COBYLA, L_BFGS_B, NELDER_MEAD
-from qiskit_machine_learning.algorithms import VQC
+from qiskit_machine_learning.algorithms import NeuralNetworkClassifier
 
-# pylint: disable=redefined-outer-name, invalid-name, attribute-defined-outside-init
-from .base_classifier_benchmark import BaseClassifierBenchmark
+from .base_classifier_benchmark import DATASET_SYNTHETIC_CLASSIFICATION, DATASET_IRIS_CLASSIFICATION
+from .vqc_base_benchmark import VqcBaseClassifierBenchmark
 
 
-class VqcFitBenchmarks(BaseClassifierBenchmark):
+class VqcFitBenchmarks(VqcBaseClassifierBenchmark):
     """Variational Quantum Classifier benchmarks."""
 
-    version = 1
+    version = 2
     timeout = 1200.0
     params = (
-        ["dataset_1"],
+        # Only the synthetic dataset now
+        [DATASET_SYNTHETIC_CLASSIFICATION],
         ["qasm_simulator", "statevector_simulator"],
         ["cobyla", "nelder-mead", "l-bfgs-b"],
         ["cross_entropy", "squared_error"],
     )
-    param_names = ["backend name", "optimizer", "loss function"]
+    param_names = ["dataset", "backend name", "optimizer", "loss function"]
 
-    def __init__(self):
+    def __init__(self) -> None:
         super().__init__()
 
-        self.optimizers = {"cobyla": COBYLA(), "nelder-mead": NELDER_MEAD(), "l-bfgs-b": L_BFGS_B()}
+        self.optimizers = {
+            "cobyla": COBYLA(maxiter=100),
+            "nelder-mead": NELDER_MEAD(maxiter=50),
+            "l-bfgs-b": L_BFGS_B(maxiter=20),
+        }
+        self.train_features: Optional[np.ndarray] = None
+        self.train_labels: Optional[np.ndarray] = None
+        self.test_features: Optional[np.ndarray] = None
+        self.test_labels: Optional[np.ndarray] = None
+        self.model: Optional[NeuralNetworkClassifier] = None
 
-    def setup(self, dataset, quantum_instance_name, optimizer_name, loss_name):
-        """setup"""
-        self.X = self.datasets[dataset]
-        num_inputs = len(self.X[0])
-        num_samples = len(self.X)
-        y01 = 1 * (np.sum(self.X, axis=1) >= 0)  # in { 0,  1}
-        self.y_one_hot = np.zeros((num_samples, 2))
-        for i in range(num_samples):
-            self.y_one_hot[i, y01[i]] = 1
-        # construct feature map, ansatz, and optimizer
-        feature_map = ZZFeatureMap(num_inputs)
-        ansatz = RealAmplitudes(num_inputs, reps=1)
+    def setup(
+        self, dataset: str, quantum_instance_name: str, optimizer: str, loss_function: str
+    ) -> None:
+        """Set up the benchmark."""
+        self.train_features = self.datasets[dataset]["train_features"]
+        self.train_labels = self.datasets[dataset]["train_labels"]
 
-        # construct variational quantum classifier
-        self.vqc = VQC(
-            feature_map=feature_map,
-            ansatz=ansatz,
-            loss=loss_name,
-            optimizer=self.optimizers[optimizer_name],
-            quantum_instance=self.backends[quantum_instance_name],
-        )
+        if dataset == DATASET_SYNTHETIC_CLASSIFICATION:
+            self.model = self._construct_vqc_classifier_synthetic(
+                quantum_instance_name=quantum_instance_name,
+                optimizer=self.optimizers[optimizer],
+                loss_function=loss_function,
+            )
+        elif dataset == DATASET_IRIS_CLASSIFICATION:
+            self.model = self._construct_vqc_classifier_iris(
+                quantum_instance_name=quantum_instance_name,
+                optimizer=self.optimizers[optimizer],
+                loss_function=loss_function,
+            )
+        else:
+            raise ValueError(f"Unsupported dataset: {dataset}")
 
+    # pylint: disable=invalid-name
     def time_fit_vqc(self, _, __, ___, ____):
         """Time fitting VQC to data."""
-
-        self.vqc.fit(self.X, self.y_one_hot)
+        self.model.fit(self.train_features, self.train_labels)
 
 
 if __name__ == "__main__":
-    for dataset, backend, optimizer, loss_function in product(*VqcFitBenchmarks.params):
+    for dataset_name, backend_name, optimizer_name, loss_function_name in product(
+        *VqcFitBenchmarks.params
+    ):
         bench = VqcFitBenchmarks()
         try:
-            bench.setup(dataset, backend, optimizer, loss_function)
+            bench.setup(dataset_name, backend_name, optimizer_name, loss_function_name)
         except NotImplementedError:
             continue
 
         for method in ["time_fit_vqc"]:
             elapsed = timeit(
-                f"bench.{method}(None, None, None, None)", number=10, globals=globals()
+                f'bench.{method}("{dataset_name}", "{backend_name}", '
+                f'"{optimizer_name}", "{loss_function_name}")',
+                number=10,
+                globals=globals(),
             )
             print(f"{method}:\t{elapsed}")
