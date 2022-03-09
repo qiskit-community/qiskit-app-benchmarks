f374b4d2116b1c80b1d6405e0c66931f9747db0f
Manoel Marques <manoel.marques@ibm.com>
2022-03-09 13:41:19 -0500

    Add ML unit tests

--- tools/cron_script.sh
+++ tools/cron_script.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # This code is part of Qiskit.
 #
-# (C) Copyright IBM 2021.
+# (C) Copyright IBM 2021, 2022.
 #
 # This code is licensed under the Apache License, Version 2.0. You may
 # obtain a copy of this license in the LICENSE.txt file in the root directory
@@ -11,142 +11,49 @@
 # copyright notice, and modified files need to carry a notice indicating
 # that they have been altered from the originals.
 
-# A virtual env names benchmark has been created
-# and has all the qiskit-app-benchmarks requirements-dev.txt
-# dependencies installed
+# A virtual env named benchmark has been created already
+
+# Script parameters
+CRON_BASENAME=$0
+GIT_OWNER=$1
+GIT_USERID=$2
+GIT_PERSONAL_TOKEN=$3
+
+CRON_SCRIPT_PATH=$(dirname "$CRON_BASENAME")
 
 # lock file with this file name and containing the pid
-LOCKFILE=/tmp/`basename $0`.lock
+CRON_LOCKFILE=/tmp/`basename $CRON_BASENAME`.lock
 
-if [ -f $LOCKFILE ]; then
-  if ps -p `cat $LOCKFILE` > /dev/null 2>&1; then
-      echo "Script $0 is still running."
+if [ -f $CRON_LOCKFILE ]; then
+  if ps -p `cat $CRON_LOCKFILE` > /dev/null 2>&1; then
+      echo "Script $CRON_BASENAME is still running."
+      echo "End of $CRON_BASENAME script."
       exit 0
   fi
 fi
-echo 'Start script ...'
-echo $$ > $LOCKFILE
+echo $$ > $CRON_LOCKFILE
+echo "Start script $CRON_BASENAME."
 
 # Removes the file if:
 # EXIT - normal termination
 # SIGHUP - termination of the controlling process
 # SIGKILL - immediate program termination
 # SIGINT - program interrupt INTR character
-# SIGQUIT - program interrupt QUIT characte
+# SIGQUIT - program interrupt QUIT character
 # SIGTERM - program termination by kill
-trap 'rm -f "$LOCKFILE" >/dev/null 2>&1' EXIT HUP KILL INT QUIT TERM
+trap 'rm -f "$CRON_LOCKFILE" >/dev/null 2>&1' EXIT HUP KILL INT QUIT TERM
 
-# find if asv is installed
-ASV_CMD="asv"
-if command -v $ASV_CMD > /dev/null 2>&1; then
-  echo "asv command is available in known paths."
-else
-  ASV_CMD="/usr/local/bin/asv"
-  if command -v $ASV_CMD > /dev/null 2>&1; then
-    echo "asv command is available at $ASV_CMD"
-  else
-    echo "asv command not found in any known path."
-    exit 1
-  fi
-fi
-
-echo "echo $3" > /tmp/.git-askpass
-chmod +x /tmp/.git-askpass
-export GIT_ASKPASS=/tmp/.git-askpass
-
+echo 'Activate environment'
 source /opt/benchmark/bin/activate
 
-set -e
-
-echo 'qiskit-app-benchmarks was already cloned in opt and is checkout to main branch'
-echo 'qiskit-app-benchmarks has a gh-pages branch with the html contents in it'
-
-rm -rf /tmp/qiskit-app-benchmarks
-
-git clone https://$2@github.com/$1/qiskit-app-benchmarks.git /tmp/qiskit-app-benchmarks
-
+echo 'Update benchmarks repository dependencies'
 git pull
-make clean_sphinx
-make html SPHINXOPTS=-W
+pip install -U -r requirements-dev.txt
 
-echo 'Copy main docs'
+echo 'Run ML Unit tests script'
+. $CRON_SCRIPT_PATH/ml_unittests.sh $GIT_PERSONAL_TOKEN
 
-pushd /tmp/qiskit-app-benchmarks
-git config user.name "Qiskit Application Benchmarks Autodeploy"
-git config user.email "qiskit@qiskit.org"
-git checkout gh-pages
-GLOBIGNORE=.git:finance:machine_learning:nature:optimization
-rm -rf * .*
-unset GLOBIGNORE
-popd
+echo 'Run benchmarks script'
+. $CRON_SCRIPT_PATH/benchmarks.sh $GIT_OWNER $GIT_USERID $GIT_PERSONAL_TOKEN
 
-declare -a targets=("finance" "nature" "optimization" "machine_learning")
-
-# copy base html to benchmarks gh-pages branch
-rm -rf /tmp/qiskit-app-benchmarks-html
-mkdir /tmp/qiskit-app-benchmarks-html
-cp -r docs/_build/html/. /tmp/qiskit-app-benchmarks-html
-for target in "${targets[@]}"
-do
-  rm -rf /tmp/qiskit-app-benchmarks-html/$target
-done
-cp -r /tmp/qiskit-app-benchmarks-html/. /tmp/qiskit-app-benchmarks
-
-
-pushd /tmp/qiskit-app-benchmarks
-git add .
-# push only if there are changes
-if git diff-index --quiet HEAD --; then
-  echo 'Nothing to commit for the base doc template.'
-else
-  git commit -m "[Benchmarks] Base documentation update"
-fi
-popd
-
-echo 'Run Benchmarks for domains'
-for target in "${targets[@]}"
-do
-  pushd $target
-  if [ -n "$(find benchmarks/* -not -name '__*' | head -1)" ]; then
-    date
-    asv_result=0
-    if [ -z "$ASV_QUICK" ]; then
-      echo "Run Benchmarks for domain $target"
-      $ASV_CMD run --show-stderr --launch-method spawn --record-samples NEW && asv_result=$? || asv_result=$?
-    else
-      echo "Run Quick Benchmarks for domain $target"
-      $ASV_CMD run --quick --show-stderr && asv_result=$? || asv_result=$?
-    fi
-    date
-    echo "asv command returned $asv_result for domain $target"
-    if [ $asv_result == 0 ]; then
-      echo "Publish Benchmark for domain $target"
-      $ASV_CMD publish
-      rm -rf /tmp/qiskit-app-benchmarks/$target/*
-      cp -r .asv/html/. /tmp/qiskit-app-benchmarks/$target
-    fi
-  else
-    rm -rf /tmp/qiskit-app-benchmarks/$target/*
-    cp -r ../docs/_build/html/$target/. /tmp/qiskit-app-benchmarks/$target
-    echo "No Benchmark files found for domain $target, run skipped."
-  fi
-  popd
-  pushd /tmp/qiskit-app-benchmarks
-  git add .
-  # push only if there are changes
-  if git diff-index --quiet HEAD --; then
-    echo "Nothing to push for $target."
-  else
-    echo "Push benchmark for $target."
-    git commit -m "[Benchmarks $target] Automated documentation update"
-    git push origin gh-pages
-  fi
-  popd
-done
-
-echo 'Final Cleanup'
-unset GIT_ASKPASS
-rm /tmp/.git-askpass
-rm -rf /tmp/qiskit-app-benchmarks
-rm -rf /tmp/qiskit-app-benchmarks-html
-echo 'End of script.'
+echo "End of $CRON_BASENAME script."
