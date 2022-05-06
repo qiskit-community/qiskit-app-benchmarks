1efe03f897d35cd0e43f4e5d24d3e6f569d49527
Md. Abrar Jahin <56369280+Abrar2652@users.noreply.github.com>
2022-05-06 20:57:07 +0600

    Update pegasosQsvc_benchmark.py

--- machine_learning/benchmarks/pegasosQsvc_benchmark.py
+++ machine_learning/benchmarks/pegasosQsvc_benchmark.py
@@ -45,7 +45,7 @@ class PegasosQsvcBenchmarks(PegasosQsvcBaseClassifierBenchmark):
         self.train_labels: Optional[np.ndarray] = None
         self.test_features: Optional[np.ndarray] = None
         self.test_labels: Optional[np.ndarray] = None
-        self.model: Optional[QuantumKernel] = None #ask here model is declared, and called through setup
+        self.model: Optional[QuantumKernel] = None 
 
     def setup(self, dataset: str, quantum_instance_name: str) -> None:
         """Set up the benchmark."""
@@ -53,8 +53,7 @@ class PegasosQsvcBenchmarks(PegasosQsvcBaseClassifierBenchmark):
         self.train_labels = self.datasets[dataset]["train_labels"]
         self.test_features = self.datasets[dataset]["test_features"]
         self.test_labels = self.datasets[dataset]["test_labels"]
-        #here I don't care about dataset, I just put the "num_qubits" in _construct_Q...()
-        #I need to test but I bet is self.train_feature.shape[1] rofl
+
         n_qubits = self.train_features.shape[1]
         if dataset == DATASET_SYNTHETIC_CLASSIFICATION:
             _kernel = self._construct_QuantumKernel_classical_classifier(quantum_instance_name= quantum_instance_name, 
@@ -87,13 +86,13 @@ class PegasosQsvcBenchmarks(PegasosQsvcBaseClassifierBenchmark):
                 _kernel = self._construct_QuantumKernel_classical_classifier(quantum_instance_name= backend, 
                                                                              optimizer = COBYLA(maxiter=200), 
                                                                              num_qubits = n_qubits)
-                model = PegasosQSVC(kernel = _kernel.evaluate)
+                model = _kernel
                 
             elif dataset == DATASET_IRIS_CLASSIFICATION:
                 _kernel = self._construct_QuantumKernel_classical_classifier(quantum_instance_name= backend, 
                                                                              optimizer = COBYLA(maxiter=200), 
                                                                              num_qubits = n_qubits)
-                model = PegasosQSVC(kernel = _kernel.evaluate)
+                model = _kernel
             else:
                 raise ValueError(f"Unsupported dataset: {dataset}")              
           
@@ -105,29 +104,29 @@ class PegasosQsvcBenchmarks(PegasosQsvcBaseClassifierBenchmark):
     # pylint: disable=invalid-name
     def time_score_PegasosQsvc_classifier(self, _, __):
         """Time scoring PegasosQsvc on data."""
-        self.model.score(self.train_features, self.train_labels)
+        PegasosQSVC(kernel = self.model).score(self.train_features, self.train_labels)
 
     def time_predict_PegasosQsvc_classifier(self, _, __):
         """Time predicting with PegasosQsvc."""
-        self.model.predict(self.train_features)
+        PegasosQSVC(kernel = self.model).predict(self.train_features)
 
     def track_accuracy_score_PegasosQsvc_classifier(self, _, __):
         """Tracks the overall accuracy of the classification results."""
-        return self.model.score(self.test_features, self.test_labels)
+        return PegasosQSVC(kernel = self.model).score(self.test_features, self.test_labels)
 
     def track_precision_score_PegasosQsvc_classifier(self, _, __):
         """Tracks the precision score."""
-        predicts = self.model.predict(self.test_features)
+        predicts = PegasosQSVC(kernel = self.model).predict(self.test_features)
         return precision_score(y_true=self.test_labels, y_pred=predicts, average="micro")
 
     def track_recall_score_PegasosQsvc_classifier(self, _, __):
         """Tracks the recall score for each class of the classification results."""
-        predicts = self.model.predict(self.test_features)
+        predicts = PegasosQSVC(kernel = self.model).predict(self.test_features)
         return recall_score(y_true=self.test_labels, y_pred=predicts, average="micro")
 
     def track_f1_score_PegasosQsvc_classifier(self, _, __):
         """Tracks the f1 score for each class of the classification results."""
-        predicts = self.model.predict(self.test_features)
+        predicts = PegasosQSVC(kernel = self.model).predict(self.test_features)
         return f1_score(y_true=self.test_labels, y_pred=predicts, average="micro")
 
 if __name__ == "__main__":
