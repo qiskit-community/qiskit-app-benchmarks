31783afe759d3f84a5d1e9d5f684afa9eee36b29
Manoel Marques <manoel.marques@ibm.com>
2022-06-16 17:12:49 -0400

    Fix lint, spell, mypy

--- nature/benchmarks/jordan_wigner_benchmark.py
+++ nature/benchmarks/jordan_wigner_benchmark.py
@@ -11,6 +11,7 @@
 # that they have been altered from the originals.
 
 """Jordan-Wigner Mapper Benchmarks."""
+from typing import Union, List
 from timeit import timeit
 from itertools import product
 
@@ -20,38 +21,50 @@ from qiskit_nature.drivers import UnitsType
 from qiskit_nature.drivers.second_quantization import PySCFDriver
 from qiskit_nature.problems.second_quantization.electronic import ElectronicStructureProblem
 
+# pylint: disable=redefined-outer-name, invalid-name, attribute-defined-outside-init
+
+
 class JordanWignerMapperBenchmarks:
-    """Jordan Wigner Mapper Benchamrks"""
+    """Jordan Wigner Mapper Benchmarks"""
 
     version = 1
-    params = [[i for i in range(7)], ["dense", "sparse"]]
+    params: List[Union[List[int], List[str]]] = [list(range(7)), ["dense", "sparse"]]
     param_names = ["op_number", "display_format"]
 
     def setup_cache(self):
-        _driver = PySCFDriver(atom='H .0 .0 .0; H .0 .0 0.735',
-                              unit=UnitsType.ANGSTROM,
-                              basis='sto3g')
+        """setup cache"""
+        _driver = PySCFDriver(
+            atom="H .0 .0 .0; H .0 .0 0.735", unit=UnitsType.ANGSTROM, basis="sto3g"
+        )
         _problem = ElectronicStructureProblem(_driver)
         second_q_ops_list = _problem.second_q_ops()
         return second_q_ops_list
 
     def setup(self, second_q_ops_list, _, __):
+        """setup"""
         self.second_q_ops_list = second_q_ops_list
         self.jw_mapper = JordanWignerMapper()
 
     def time_map(self, _, op_number, __):
+        """time map"""
         return self.jw_mapper.map(self.second_q_ops_list[op_number])
 
+
 if __name__ == "__main__":
+    bench = JordanWignerMapperBenchmarks()
+    second_q_ops_list = bench.setup_cache()
     for op_number, display_format in product(*JordanWignerMapperBenchmarks.params):
         bench = JordanWignerMapperBenchmarks()
-        second_q_ops_list = bench.setup_cache()
         try:
             bench.setup(second_q_ops_list, op_number, display_format)
         except NotImplementedError:
             continue
 
-    for method in set(dir(JordanWignerMapperBenchmarks)):
-        if method.startswith("time_"):
-            elapsed = timeit(f"bench.{method}", number = 10, globals = globals())
-            print(f"bench.{method} : \t{elapsed}")
+        for method in set(dir(JordanWignerMapperBenchmarks)):
+            if method.startswith("time_"):
+                elapsed = timeit(
+                    f"bench.{method}(None, {op_number}, '{display_format}')",
+                    number=10,
+                    globals=globals(),
+                )
+                print(f"bench.{method} : \t{elapsed}")
--- nature/benchmarks/linear_mapper_benchmark.py
+++ nature/benchmarks/linear_mapper_benchmark.py
@@ -14,18 +14,22 @@
 from timeit import timeit
 
 import retworkx
-from qiskit_nature.mappers.second_quantization import LinearMapper, LogarithmicMapper
+from qiskit_nature.mappers.second_quantization import LinearMapper
 from qiskit_nature.problems.second_quantization.lattice.models import IsingModel
 from qiskit_nature.problems.second_quantization.lattice import Lattice
 
+# pylint: disable=redefined-outer-name, invalid-name, attribute-defined-outside-init
+
+
 class LinearMapperBenchmarks:
-    """Linear Mapper Benchamrks"""
+    """Linear Mapper Benchmarks"""
 
     version = 1
     params = [0]
     param_names = ["op_number"]
 
     def setup_cache(self):
+        """setup cache"""
         graph = retworkx.PyGraph(multigraph=False)
         graph.add_nodes_from(list(range(20)))
         graph.add_edge(1, 5, 4)
@@ -34,12 +38,15 @@ class LinearMapperBenchmarks:
         second_q_ops = ising_model.second_q_ops()
         return second_q_ops
 
-    def setup(self, second_q_ops, op_number):
+    def setup(self, second_q_ops, __):
+        """setup"""
         self.second_q_ops = second_q_ops
         self.linear_mapper = LinearMapper()
 
     def time_map(self, _, __):
+        """time map"""
         return self.linear_mapper.map(self.second_q_ops)
+
 
 if __name__ == "__main__":
     bench = LinearMapperBenchmarks()
@@ -51,7 +58,7 @@ if __name__ == "__main__":
         except NotImplementedError:
             continue
 
-    for method in set(dir(LinearMapperBenchmarks)):
-        if method.startswith("time_"):
-            elapsed = timeit(f"bench.{method}", number = 10, globals = globals())
-            print(f"bench.{method} : \t{elapsed}")
+        for method in set(dir(LinearMapperBenchmarks)):
+            if method.startswith("time_"):
+                elapsed = timeit(f"bench.{method}(None, {op_number})", number=10, globals=globals())
+                print(f"bench.{method} : \t{elapsed}")
