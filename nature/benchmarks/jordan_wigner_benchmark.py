9df8df1e598de59b39e515bf292fdc566bfd6f49
SaashaJoshi <saashajoshi@ibm.com>
2022-07-06 21:34:11 -0400

    modifications to linear and jordan wigner mapper benchmarks

--- nature/benchmarks/jordan_wigner_benchmark.py
+++ nature/benchmarks/jordan_wigner_benchmark.py
@@ -11,15 +11,17 @@
 # that they have been altered from the originals.
 
 """Jordan-Wigner Mapper Benchmarks."""
-from typing import Union, List
+import argparse
+from typing import List
 from timeit import timeit
-from itertools import product
-
+from pathlib import Path
+from qiskit_nature import settings
+from qiskit_nature.hdf5 import save_to_hdf5
+from qiskit_nature.drivers import UnitsType
+from qiskit_nature.drivers.second_quantization import PySCFDriver, HDF5Driver
 from qiskit_nature.mappers.second_quantization import JordanWignerMapper
 
-from qiskit_nature.drivers import UnitsType
-from qiskit_nature.drivers.second_quantization import PySCFDriver
-from qiskit_nature.problems.second_quantization.electronic import ElectronicStructureProblem
+settings.dict_aux_operators = True
 
 # pylint: disable=redefined-outer-name, invalid-name, attribute-defined-outside-init
 
@@ -28,51 +30,105 @@ class JordanWignerMapperBenchmarks:
     """Jordan-Wigner Mapper Benchmarks."""
 
     version = 1
-    params: List[Union[List[int], List[str]]] = [list(range(3 * 7)), ["dense", "sparse"]]
-    param_names = ["op_number", "display_format"]
+    params: List[str] = [
+        "H2 ParticleNumber",
+        "H2 ElectronicEnergy",
+        "H2 DipoleMomentX",
+        "H2 DipoleMomentY",
+        "H2 DipoleMomentZ",
+        "H2 AngularMomentum",
+        "H2 Magnetization",
+        "H2O ParticleNumber",
+        "H2O ElectronicEnergy",
+        "H2O DipoleMomentX",
+        "H2O DipoleMomentY",
+        "H2O DipoleMomentZ",
+        "H2O AngularMomentum",
+        "H2O Magnetization",
+        "LiH ParticleNumber",
+        "LiH ElectronicEnergy",
+        "LiH DipoleMomentX",
+        "LiH DipoleMomentY",
+        "LiH DipoleMomentZ",
+        "LiH AngularMomentum",
+        "LiH Magnetization",
+    ]
+    param_names = ["operator_type"]
+
+    _hdf5_files = [
+        ("H .0 .0 .0; H .0 .0 0.735", "jordan_wigner_benchmark_driver_H2.hdf5"),
+        (
+            "O 0.0 0.0 0.0; H 0.758602 0.0 0.504284; H 0.758602 0.0 -0.504284",
+            "jordan_wigner_benchmark_driver_H2O.hdf5",
+        ),
+        ("Li 0.0 0.0 0.0; H 0.0 0.0 1.5474", "jordan_wigner_benchmark_driver_LiH.hdf5"),
+    ]
+
+    @staticmethod
+    def make_hdf5_file():
+        """create hdf5 files"""
+
+        for _, (atom, file_name) in enumerate(JordanWignerMapperBenchmarks._hdf5_files):
+            _driver = PySCFDriver(
+                atom=atom,
+                unit=UnitsType.ANGSTROM,
+                basis="sto3g",
+            )
+            _molecule = _driver.run()
+            save_to_hdf5(_molecule, file_name, replace=True)
 
     def setup_cache(self):
         """setup cache"""
-        atom_list = [
-            "H .0 .0 .0; H .0 .0 0.735",
-            "O 0.0 0.0 0.0; H 0.758602 0.0 0.504284; H 0.758602 0.0 -0.504284",
-            "Li 0.0 0.0 0.0; H 0.0 0.0 1.5474",
-        ]
-        second_q_ops_list = []
-        for num_atom in range(3):
-            _driver = PySCFDriver(atom=atom_list[num_atom], unit=UnitsType.ANGSTROM, basis="sto3g")
-            _problem = ElectronicStructureProblem(_driver)
-            second_q_ops_list.append(_problem.second_q_ops())
-            print(len(second_q_ops_list))
 
-        second_q_ops_list = [second_q_ops for atoms in second_q_ops_list for second_q_ops in atoms]
+        source_path = Path(__file__).resolve()
+        source_dir = source_path.parent
+        second_q_ops_list = []
+        for _, file_name in JordanWignerMapperBenchmarks._hdf5_files:
+            file_path = Path(source_dir, file_name)
+            _hdf5_driver = HDF5Driver(file_path.resolve())
+            _molecule = _hdf5_driver.run()
+            atom_ops_list = list(_molecule.second_q_ops().values())
+            for item in atom_ops_list:
+                second_q_ops_list.append(item)
+
         return second_q_ops_list
 
-    def setup(self, second_q_ops_list, _, __):
+    def setup(self, second_q_ops_list, operator):
         """setup"""
+        self.op_number = self.params.index(operator)
         self.second_q_ops_list = second_q_ops_list
         self.jw_mapper = JordanWignerMapper()
 
-    def time_map(self, _, op_number, __):
+    def time_map(self, _, __):
         """time map"""
-        return self.jw_mapper.map(self.second_q_ops_list[op_number])
+        return self.jw_mapper.map(self.second_q_ops_list[self.op_number])
 
 
 if __name__ == "__main__":
-    bench = JordanWignerMapperBenchmarks()
-    second_q_ops_list = bench.setup_cache()
-    for op_number, display_format in product(*JordanWignerMapperBenchmarks.params):
-        bench = JordanWignerMapperBenchmarks()
-        try:
-            bench.setup(second_q_ops_list, op_number, display_format)
-        except NotImplementedError:
-            continue
+    PARSER = argparse.ArgumentParser(description="Create hdf5 files")
+    PARSER.add_argument(
+        "-hdf5create", action="store_true", help="creates hdf5 file with PYSCFDriver results"
+    )
 
-        for method in set(dir(JordanWignerMapperBenchmarks)):
-            if method.startswith("time_"):
-                elapsed = timeit(
-                    f"bench.{method}(None, {op_number}, '{display_format}')",
-                    number=10,
-                    globals=globals(),
-                )
-                print(f"bench.{method} : \t{elapsed}")
+    ARGS = PARSER.parse_args()
+    if ARGS.hdf5create:
+        JordanWignerMapperBenchmarks.make_hdf5_file()
+    else:
+        bench = JordanWignerMapperBenchmarks()
+        second_q_ops_list = bench.setup_cache()
+        for operator in JordanWignerMapperBenchmarks.params:
+            bench = JordanWignerMapperBenchmarks()
+            try:
+                bench.setup(second_q_ops_list, operator)
+            except NotImplementedError:
+                continue
+
+            for method in set(dir(JordanWignerMapperBenchmarks)):
+                if method.startswith("time_"):
+                    elapsed = timeit(
+                        f"bench.{method}(None, '{operator}')",
+                        number=10,
+                        globals=globals(),
+                    )
+                    print(f"bench.{method} : \t{elapsed}")
+                continue
--- nature/benchmarks/linear_mapper_benchmark.py
+++ nature/benchmarks/linear_mapper_benchmark.py
@@ -13,7 +13,6 @@
 """Linear Mapper Benchmarks."""
 from timeit import timeit
 import numpy as np
-
 import retworkx
 from qiskit_nature.mappers.second_quantization import LinearMapper
 from qiskit_nature.problems.second_quantization.lattice.models import IsingModel
@@ -26,24 +25,127 @@ class LinearMapperBenchmarks:
     """Linear Mapper Benchmarks."""
 
     version = 1
-    seed = 100
-    params = list(range(3))
-    param_names = ["op_number"]
+    timeout = 120.0
+    params = [50, 80, 100]
+    param_names = ["Number of nodes"]
 
     def setup_cache(self):
         """setup cache"""
 
+        np.random.seed(100)
         second_q_ops_list = []
+        edge_arr = [
+            24,
+            67,
+            87,
+            79,
+            48,
+            10,
+            94,
+            52,
+            98,
+            53,
+            66,
+            98,
+            14,
+            34,
+            24,
+            15,
+            60,
+            58,
+            16,
+            9,
+            93,
+            86,
+            2,
+            27,
+            4,
+            31,
+            1,
+            13,
+            83,
+            4,
+            91,
+            59,
+            67,
+            7,
+            49,
+            47,
+            65,
+            61,
+            14,
+            55,
+            71,
+            80,
+            2,
+            94,
+            19,
+            98,
+            63,
+            53,
+            27,
+            56,
+            30,
+            48,
+            47,
+            39,
+            38,
+            44,
+            18,
+            64,
+            56,
+            34,
+            53,
+            74,
+            17,
+            72,
+            13,
+            30,
+            17,
+            53,
+            68,
+            50,
+            91,
+            91,
+            83,
+            53,
+            78,
+            0,
+            13,
+            57,
+            76,
+            3,
+            70,
+            3,
+            84,
+            79,
+            10,
+            87,
+            60,
+            3,
+            48,
+            52,
+            43,
+            36,
+            5,
+            71,
+            38,
+            86,
+            94,
+            98,
+            42,
+            84,
+        ]
 
-        for _ in range(3):
-            num_nodes = np.random.randint(40)
+        for index, _ in enumerate(self.params):
             graph = retworkx.PyGraph(multigraph=False)
-            graph.add_nodes_from(list(range(num_nodes)))
+            graph.add_nodes_from(list(range(self.params[index])))
 
-            for i in range(num_nodes):
-                for j in range(num_nodes):
-                    if j != i:
-                        graph.add_edge(i, j, np.random.randint(100))
+            count = 0
+            for i in range(self.params[index]):
+                for j in range(i + 1, self.params[index]):
+                    count += 1
+                    graph.add_edge(i, j, edge_arr[i])
 
             lattice = Lattice(graph)
             ising_model = IsingModel(lattice)
@@ -52,27 +154,28 @@ class LinearMapperBenchmarks:
 
         return second_q_ops_list
 
-    def setup(self, second_q_ops_list, __):
+    def setup(self, second_q_ops_list, num_nodes):
         """setup"""
         self.second_q_ops_list = second_q_ops_list
+        self.op_number = self.params.index(num_nodes)
         self.linear_mapper = LinearMapper()
 
-    def time_map(self, _, op_number):
+    def time_map(self, _, __):
         """time map"""
-        return self.linear_mapper.map(self.second_q_ops_list[op_number])
+        return self.linear_mapper.map(self.second_q_ops_list[self.op_number])
 
 
 if __name__ == "__main__":
     bench = LinearMapperBenchmarks()
     second_q_ops_list = bench.setup_cache()
-    for op_number in LinearMapperBenchmarks.params:
+    for num_nodes in LinearMapperBenchmarks.params:
         bench = LinearMapperBenchmarks()
         try:
-            bench.setup(second_q_ops_list, op_number)
+            bench.setup(second_q_ops_list, num_nodes)
         except NotImplementedError:
             continue
 
         for method in set(dir(LinearMapperBenchmarks)):
             if method.startswith("time_"):
-                elapsed = timeit(f"bench.{method}(None, {op_number})", number=10, globals=globals())
+                elapsed = timeit(f"bench.{method}(None, {num_nodes})", number=10, globals=globals())
                 print(f"bench.{method} : \t{elapsed}")
